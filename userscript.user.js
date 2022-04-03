b8e91742e1c9724db8b53012d992d45b5abedfca
lovelykohai <60541661+lovelykohai@users.noreply.github.com>
2022-04-03 23:13:35 +0100

    Update userscript.user.js

--- userscript.user.js
+++ userscript.user.js
@@ -13,7 +13,7 @@ if (window.top !== window.self) {
             document.getElementsByTagName("mona-lisa-embed")[0].shadowRoot.children[0].getElementsByTagName("mona-lisa-canvas")[0].shadowRoot.children[0].appendChild(
         (function () {
             const i = document.createElement("img");
-            i.src = "https://i.redd.it/n5rcotzlzdr81.png";
+            i.src = "https://i.redd.it/1fyqdi4d1er81.png";
             i.style = "position: absolute;left: 0;top: 0;image-rendering: pixelated;width: 2000px;height: 2000px;";
             console.log(i);
             return i;
